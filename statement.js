4239083a76bb03f583ea6830433beae5fccc1b4a
hyeonmi <pilotfish22@gmail.com>
2020-06-09 10:32:59 +0900

    extract totalVolumeCredits

--- statement.js
+++ statement.js
@@ -41,21 +41,25 @@ function usd(number){
   return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(number/100);
 }
 
+function totalVolumeCredits(invoice){
+  let volumeCredits = 0;
+  for(let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
+}
+
 function statement(invoice){
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역(고객명: ${invoice.customer})\n`;
 
-
   for(let perf of invoice.performances){
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역 출력
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석) \n`;
     totalAmount += amountFor(perf)
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
+  result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
   return result;
 }
