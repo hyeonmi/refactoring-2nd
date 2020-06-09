9e4f0b2e479cfa45303f70d41f8d921f307f953e
hyeonmi <pilotfish22@gmail.com>
2020-06-09 10:00:55 +0900

    extract amountFor

--- statement.js
+++ statement.js
@@ -1,3 +1,26 @@
+function amountFor(perf, play){
+  let thisAmount = 0;
+
+  switch(play.type){
+    case "tragedy":
+      thisAmount = 40000;
+      if(perf.audience > 30){
+        thisAmount += 1000*  (perf.audience  - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if(perf.audience > 20){
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르 : ${play.type}`);
+  }
+  return thisAmount
+}
 function statement(invoice, plays){
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -6,26 +29,8 @@ function statement(invoice, plays){
 
   for(let perf of invoice.performances){
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    const thisAmount = amountFor(perf, play);
 
-    switch(play.type){
-      case "tragedy":
-        thisAmount = 40000;
-        if(perf.audience > 30){
-          thisAmount += 1000*  (perf.audience  - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if(perf.audience > 20){
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
 
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -40,6 +45,5 @@ function statement(invoice, plays){
 
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
-  console.log(result);
   return result;
 }
