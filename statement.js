0a945527f228ed9c5d78ce9b387a023e27d519ae
hyeonmi <pilotfish22@gmail.com>
2020-06-09 10:09:27 +0900

    rename thisAmount to result

--- statement.js
+++ statement.js
@@ -1,25 +1,25 @@
-function amountFor(perf, play){
-  let thisAmount = 0;
+function amountFor(performance, play){
+  let result = 0;
 
   switch(play.type){
     case "tragedy":
-      thisAmount = 40000;
-      if(perf.audience > 30){
-        thisAmount += 1000*  (perf.audience  - 30);
+      result = 40000;
+      if(performance.audience > 30){
+        result += 1000 *  (performance.audience  - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if(perf.audience > 20){
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if(performance.audience > 20){
+        result += 10000 + 500 * (performance.audience - 20);
       }
 
-      thisAmount += 300 * perf.audience;
+      result += 300 * performance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르 : ${play.type}`);
   }
-  return thisAmount
+  return result
 }
 function statement(invoice, plays){
   let totalAmount = 0;
