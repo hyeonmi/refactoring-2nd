06e4975416c2496421191667e97a1105c770c677
hyeonmi <pilotfish22@gmail.com>
2020-06-09 10:28:23 +0900

    extract usd

--- statement.js
+++ statement.js
@@ -37,21 +37,25 @@ function volumeCreditsFor (performance) {
   return result;
 }
 
+function usd(number){
+  return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(number/100);
+}
+
 function statement(invoice){
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역(고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
+
 
   for(let perf of invoice.performances){
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역 출력
-    result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석) \n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석) \n`;
     totalAmount += amountFor(perf)
   }
 
-  result += `총액: ${format(totalAmount/100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
