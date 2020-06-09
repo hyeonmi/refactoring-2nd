18edb0a73375e4ca1c1fb40985e4ea0d268148fd
hyeonmi <pilotfish22@gmail.com>
2020-06-09 11:31:34 +0900

    add html render function and render html

--- statement.js
+++ statement.js
@@ -19,3 +19,27 @@ function renderPlainText(data){
 function statement(invoice, plays){
   return renderPlainText(createStatementData(invoice, plays));
 }
+
+function renderHtml(data){
+  let result = `<h1>청구 내역(고객명: ${data.customer})</h1>`;
+  result += `<table>
+                <tr>
+                    <th>연극</th><th>좌석 수</th><th>금액</th>
+                </tr>`;
+  for(let perf of data.performances){
+    result += `<tr>
+                <td>${perf.play.name}</td>
+                <td>${perf.audience}석</td>
+                <td>${usd(perf.amount)}</td>
+              </tr>`;
+  }
+  result += `</table>`
+
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>`;
+  return result;
+}
+
+function htmlStatement(invoice, plays){
+  return renderHtml(createStatementData(invoice, plays));
+}
