f819cde9704dc5eac8a42f3d3103d3d7fae6e5c7
hyeonmi <pilotfish22@gmail.com>
2020-06-09 10:17:29 +0900

    remove thisAmount variable

--- statement.js
+++ statement.js
@@ -33,8 +33,6 @@ function statement(invoice){
   const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
   for(let perf of invoice.performances){
-    const thisAmount = amountFor(perf);
-
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -42,8 +40,8 @@ function statement(invoice){
     if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역 출력
-    result += `${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석) \n`;
-    totalAmount += thisAmount
+    result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석) \n`;
+    totalAmount += amountFor(perf)
   }
 
   result += `총액: ${format(totalAmount/100)}\n`;
