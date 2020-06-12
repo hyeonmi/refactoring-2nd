f4a2dd0d9b477f3a4f557e0a5973211a7dba27ca
hyeonmi <pilotfish22@gmail.com>
2020-06-12 10:38:17 +0900

    remove superclass amount condition
    - 서브클래스로 다 옮기고 난 후 슈퍼클래스의 조건문을 삭제하고 개발자에게 가이드 할수 있는 문장을 잠겨둔다.

--- createStatement.js
+++ createStatement.js
@@ -6,14 +6,7 @@ class PerformanceCalculator {
   }
 
   get amount(){
-    switch(this.play.type){
-      case "tragedy":
-        throw "오류 발생";
-      case "comedy":
-        throw "오류 발생";
-      default:
-        throw new Error(`알 수 없는 장르 : ${this.performance.play.type}`);
-    }
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
   }
 
   get volumeCredits(){
